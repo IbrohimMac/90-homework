108c123687d322c8bf26a17e2d3970238a22af9e
Xpanetel <ibrohim@MAC.local>
2024-05-07 23:44:08 +0500

    new

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <h1>Where in the world?</h1>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
